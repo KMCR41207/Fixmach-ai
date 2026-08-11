d30a15aa888daee9dac806359ae00cdf50a74c5f
KMCR41207 <qwertyasd077a@gmail.com>
2026-08-11 23:52:20 +0530

    perf: add dns-prefetch hint for Google Fonts

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -93,6 +93,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
+      { rel: "dns-prefetch", href: "https://fonts.googleapis.com" },
       {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Sora:wght@400;500;600;700&display=swap",
